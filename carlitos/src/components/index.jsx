947ceac599bd689347fbc6f1bf64b66dcfd8cecb
carlitos-206 <carloscaceres041@gmail,com>
2022-09-17 05:48:53 -0700

    EOD 9/17/22

--- carlitos/src/components/index.jsx
+++ carlitos/src/components/index.jsx
@@ -2,19 +2,29 @@ import React from "react";
 import { techList } from "./img/techList";
 
 export default function HomePage(){
+  let curIndex = 0;
+  let imgDuration = 1000;
+  let picture
+  const slideShow = () => {
+      picture = techList[curIndex];
+      curIndex++;
+      if (curIndex === techList.length) { 
+        curIndex = 0; }
+      setTimeout("slideShow()", imgDuration);
+  }
+  slideShow();
+  
   return(
     <div>
       <div className="banner">
         <h1>Coming Soon</h1>
       </div>
-      {techList.map((pic ,idx )=>{
-        return(
-          <img key={idx} src={pic} alt="Tech Img" style={{width: '150px', padding: '20px', borderRadius: '25%'}} />
-        )
-      })}
-        <div className="footer">
+        <img src={picture} alt="" />
+      
+      <img id="image1" />
+      <div className="footer">
           <h1>Carlos R. Caceres Martinez | <a href="https://github.com/carlitos-206" target="_blank" rel="noopener noreferrer" >GitHub</a></h1>
-        </div>
+      </div>
     </div>
   )
 }
